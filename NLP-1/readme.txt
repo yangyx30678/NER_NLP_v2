c9bbcfe583ff4563c0b8e3e25f9e3cd54ad9821f
yangyx30678 <yangyx0000001@gmail.com>
2023-10-28 04:01:54 +0800

    init

---
+++ NLP-1/readme.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7364a8e31774c823b11b1d50c8b592f685b5a53d7458347db5a253428c21d727
+size 1102
---
+++ NLP-1/test_example.py
@@ -0,0 +1,8 @@
+﻿with open('./test-submit.txt','r') as f:
+    lines = f.readlines()
+
+with open('./example.txt','w') as f:
+    for line in lines:
+        if line=='\n': f.write(line)
+        else:
+            f.write(line.replace('\n','\tO\n'))
